827671553cf62f48ef0ada51fd360d6c792e3c89
mi7an <milanbhattarai2407@gmail.com>
2023-10-02 21:35:18 +0545

    edit in user data

--- register.php
+++ register.php
@@ -2,38 +2,35 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     include("db_connection.php");
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $targeted_amount = $_POST["targamt"];
 
-   
-    $name = mysqli_real_escape_string($conn, $_POST["name"]);
-    $email = mysqli_real_escape_string($conn, $_POST["email"]);
-    $password = mysqli_real_escape_string($conn, $_POST["password"]);
-    $targeted_amount = mysqli_real_escape_string($conn, $_POST["targamt"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL) || strpos($email, '@gmail.com') === false) {
+        echo "Invalid email address. Please use a Gmail address.";
+        exit(); 
+    }
 
-    
+    if (strlen($password) < 5) {
+        echo "Password must contain at least 5 characters.";
+        exit();
+    }
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    
     $query = "INSERT INTO users (name, email, password, targeted_amount) VALUES (?, ?, ?, ?)";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("ssss", $name, $email, $hashed_password, $targeted_amount);
 
     if ($stmt->execute()) {
-        echo "<script>
-            document.getElementById('name').value = '';
-            document.getElementById('email').value = '';
-            document.getElementById('password').value = '';
-            document.getElementById('targamt').value = '';
-          </script>";
         
-        echo "Registration successful. Redirecting...";
         header("Location: index.html");
         exit(); 
     } else {
-        
+       
         echo "Registration failed. Please try again.";
     }
 
-    
     $stmt->close();
     $conn->close();
 }
